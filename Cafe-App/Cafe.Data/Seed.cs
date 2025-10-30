b17b1c3198fe568343ceced1b9d2b5148254bab3
AntaninaKapuza <antanina.kapuza@innowise.com>
2025-10-30 17:02:33 +0300

    add seed that only one admin can be existed

---
+++ Cafe-App/Cafe.Data/Seed.cs
@@ -0,0 +1,28 @@
+﻿using Cafe.Data.Interface.Repositories;
+using Cafe.Data.Models;
+using Enums.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cafe.Data;
+
+public class Seed
+{
+    public void Fill(IServiceProvider service)
+    {
+        using var di = service.CreateScope();
+
+        UserFill(di);
+    }
+
+    private void UserFill(IServiceScope di)
+    {
+        var userRepositry = di.ServiceProvider.GetRequiredService<IUserRepository<UserData>>();
+        if (userRepositry.IsAdminExist())
+        {
+            return;
+        }
+
+        userRepositry.Register("admin", "admin", Roles.Admin);
+    }
+}
+
