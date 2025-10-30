3fdfe3aea91a9ffce75c56cca4d8555c3d5cf4ff
AntaninaKapuza <antanina.kapuza@innowise.com>
2025-10-30 13:25:11 +0300

    create buisness models

---
+++ Cafe.Data/Models/ChatMessageData.cs
@@ -0,0 +1,11 @@
+﻿using Cafe.Data.Interface.Models;
+
+namespace Cafe.Data.Models
+{
+    public class ChatMessageData : BaseModel, IChatMessageData
+    {
+        public DateTime CreationTime { get; set; }
+        public string Message { get; set; } 
+        public virtual UserData? User { get; set; }
+    }
+}
---
+++ Cafe.Data/Models/UserData.cs
@@ -0,0 +1,15 @@
+﻿using Cafe.Data.Interface.Models;
+using Enums.Users;
+
+namespace Cafe.Data.Models;
+
+public class UserData : BaseModel, IUser
+{
+        public IEnumerable<CafeData>? Cafes { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string AvatarUrl { get; set; }
+        public Languages Language { get; set; }
+        public Roles Role { get; set; }
+        public virtual List<ChatMessageData> ChatMessages { get; set;} = new();
+}
